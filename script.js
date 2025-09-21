c81938f6e963b0b0282e534d5b9652a9f61b5148
karis-cod <chukwuebukaogu3@gmail.com>
2025-09-21 14:45:00 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,21 @@ const ctaConEle3 = document.querySelector('.js-cta-grid-content3');
 const ctaImgEle1 = document.querySelector('.js-cta-img-1');
 const ctaImgEle2 = document.querySelector('.js-cta-img-2');
 const ctaImgEle3 = document.querySelector('.js-cta-img-3');
+const navToggleEle = document.querySelector('.icon');
+const navToggleEle2 = document.querySelector('.icon-2');
+const navUlEle = document.querySelector('.mobile-bar');
+
+
+navToggleEle.addEventListener('click', () => {
+  navUlEle.style.display = 'block';
+  navToggleEle.style.display = 'none';
+  navToggleEle2.style.display = 'block';
+});
+navToggleEle2.addEventListener('click', () => {
+  navUlEle.style.display = 'none';
+  navToggleEle.style.display = 'block';
+  navToggleEle2.style.display = 'none';
+});
 
 ctaConEle.addEventListener('mouseenter', () => {
     ctaImgEle1.src="images/plan1_hover.png"
